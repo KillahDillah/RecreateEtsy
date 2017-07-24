b865086550dbdf6b30b4e182a76be1f992a7dc85
Leah Miller <leahmiller1969@gmail.com>
2017-07-24 09:26:45 -0700

    almost finished

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-let main = document.querySelector(".content")
+let main = document.querySelector(".results")
 let search = document.querySelector("#search")
 let form = document.querySelector('button')
 
@@ -23,13 +23,15 @@ fetch("https://api.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&
 			} 
 			response.json().then(function(data){
 				let list = ''
+				let sr = document.querySelector('.results_bar')
+				sr.innerHTML = "All Catergories "
 				data.results.forEach(function(data){
 					console.log(data)
 					list += `
 								<div class="data">
 									<img src="${data.Images[0].url_170x135}"/>
 									<li> ${data.title}</li>
-									<li> ${data.Shop.shop_name}</li>
+									<li id="shop_name"> ${data.Shop.shop_name}</li>
 									<li> $${data.price}</li>
 									<a href="${data.url}"></a>
 									
